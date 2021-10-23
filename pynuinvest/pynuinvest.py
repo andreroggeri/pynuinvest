bd61cfe90f5fadda443ef99c759ef693b739d174
André Roggeri Campos <andrerc@ciandt.com>
2021-10-23 14:47:37 -0300

    feat: Add account statements feature

--- pynuinvest/pynuinvest.py
+++ pynuinvest/pynuinvest.py
@@ -1,8 +1,11 @@
 import json
+from datetime import datetime
 from typing import Optional
 from urllib.parse import urlencode
 
 import requests
+
+BASE_URL = 'https://www.nuinvest.com.br/api'
 
 
 class NuInvest:
@@ -26,11 +29,38 @@ class NuInvest:
 
         self._auth_token = response['access_token']
 
+    def trust_device(self, device_uid: str) -> None:
+        """
+        Torna o UID 'confiável', removendo a necessidade de enviar o otp token em todos login
+        :param device_uid: ID do dispositivo
+        :return: None
+        """
+        if self._auth_token is None:
+            raise Exception('Authenticate before requesting data')
+
+        self._make_post(f'{BASE_URL}/alcatraz/trustedDevice', {
+            'deviceUid': device_uid
+        })
+
     def get_investment_data(self) -> dict:
         if self._auth_token is None:
             raise Exception('Authenticate before requesting data')
 
         return self._make_get('https://www.nuinvest.com.br/api/samwise/v2/custody-position')
+
+    def get_account_statements(self, start_date: datetime, end_date: datetime) -> dict:
+        """
+        Recupera o extrato da conta dentro do período especificado
+        :param start_date: data de inicio da consulta
+        :param end_date: data final da consulta
+        :return: Dicionário contendo informaçõe do extrato e saldo em conta
+        """
+        if self._auth_token is None:
+            raise Exception('Authenticate before requesting data')
+
+        stime = start_date.strftime('%Y-%m-%d')
+        etime = end_date.strftime('%Y-%m-%d')
+        return self._make_get(f'{BASE_URL}/gringott/statements/1?startDate={stime}&endDate={etime}')['value']
 
     def _make_post(self, url: str, inner_body: dict) -> dict:
         body = {
